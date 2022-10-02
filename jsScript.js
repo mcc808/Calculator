5e9b2c85a318aafb501fd7e3f7ee485764442f74
mcc808 <112995753+mcc808@users.noreply.github.com>
2022-10-02 00:46:02 -0700

    completed assignment

--- jsScript.js
+++ jsScript.js
@@ -1,23 +1,35 @@
-function multiplyThree(){   
-
-    let input = prompt("Please enter a number to multiply by 3");
-    let total = input * 3 
-    console.log(total);
-    document.write("The result is: " + total);
-}
-
-multiplyThree();
-
-
-
 function Calculator(){
     console.log("Calculating");
     let num1 = Number(prompt("Enter num1:"));
-    let num2 = Number(prompt("Enter num2:"));
-    sum(num1,num2);
+    let num2 = Number(prompt("Enter num2:")); 
+    mult(num1,num2);
+    div(num1,num2);
+    add(num1,num2);
+    sub(num1,num2);  
 }
 
-function sum(a,b){
-    let total = a + b;
-    document.getElementById("sumRes").innerHTML=`<p> ${a} + ${b} = ${total}</p>`;
+function mult(a,b){   
+    let total = a * b;
+    console.log(total);
+    document.getElementById("multRes").innerHTML=`<p> ${a} * ${b} = ${total}</p>`;
 }
+
+function div(a,b){
+    let total = a / b;
+    console.log(total);
+    document.getElementById("divRes").innerHTML=`<p> ${a} / ${b} = ${total}</p>`;
+}
+
+function add(a,b){
+    let total = a + b;
+    console.log(total);
+    document.getElementById("addRes").innerHTML=`<p> ${a} + ${b} = ${total}</p>`;
+}
+
+function sub(a,b){
+    let total = a - b;
+    console.log(total);
+    document.getElementById("subRes").innerHTML=`<p> ${a} - ${b} = ${total}</p>`;
+}
+
+mult();
